d279da5fc95f4a4e73a4aaa0a31e91dcf9a099ac
Subham Beura <subhambeura20@gmail.com>
2023-08-11 20:15:43 +0530

    style search bar

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { NextPage } from "next";
+import { Input } from "@/components/ui/input";
 
 interface Props {}
 
 const SearchPage: NextPage<Props> = ({}) => {
-  return <div>Search Page</div>;
+  return (
+    <div className="w-screen flex flex-col items-center space-y-6 py-14">
+      <div className="flex w-full max-w-sm items-center justify-center space-x-2">
+        <Input type="text" placeholder="Search Medicines" />
+        <Button type="submit">Search</Button>
+      </div>
+      <div>List of Medicines Found</div>
+    </div>
+  );
 };
 
 export default SearchPage;
