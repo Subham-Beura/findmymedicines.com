e81650e44719332ba2392b4c26c283eca24f03da
Subham Beura <subhambeura20@gmail.com>
2023-09-05 20:59:35 +0530

    add remove Itrems

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -12,23 +12,59 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useSession } from "next-auth/react";
+import { toast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
 interface Props {
   cartItems: any;
+  cartId: string;
 }
 
-const Cart: NextPage<Props> = ({ cartItems }) => {
+const Cart: NextPage<Props> = ({ cartItems: CI, cartId }) => {
+  const { data: session } = useSession();
+  const [cartItems, setCartItems] = useState<any>(CI);
+
   let totalPrice = 0;
+
   cartItems.map((cartItem: any, index: number) => {
     const medicineInShops: MedicineInShops = cartItem.medicinesInShops;
     totalPrice += medicineInShops.price;
   });
+
+  const removeItem = async (
+    e: any,
+    medicineInShop: MedicineInShops,
+    id: string
+  ) => {
+    console.log(medicineInShop.medicine.name);
+    try {
+      const res = await axios.delete(
+        `${process.env.NEXT_PUBLIC_API_URL}/carts/${cartId}/${id}`,
+        { headers: { Authorization: `Bearer ${session?.user.token}` } }
+      );
+      // filter cartItems such that deleted item is removed
+      setCartItems((prevState: any) => {
+        return prevState.filter((item: any) => item.id !== id);
+      });
+      // window.location.reload();
+      toast({
+        title: "Removed Item From Cart",
+      });
+    } catch (error) {
+      toast({
+        title: "OOPS! Something Went Wrong",
+        description: "Error Removing Item From Cart",
+      });
+    }
+  };
+
   return (
     <div className="p-10">
       <h1 className="scroll-m-20 text-3xl mb-10 font-extrabold tracking-tight lg:text-3xl">
         My Cart
       </h1>
-      {cartItems.lenght == 0 ? (
+      {cartItems.length == 0 ? (
         "Cart is Empty"
       ) : (
         <Table>
@@ -38,6 +74,7 @@ const Cart: NextPage<Props> = ({ cartItems }) => {
               <TableHead className="w-[300px]">Medicine</TableHead>
               <TableHead className="w-[300px]">Company</TableHead>
               <TableHead>Shop</TableHead>
+              <TableHead className="text-center">Remove</TableHead>
               <TableHead className="text-right">Amount</TableHead>
             </TableRow>
           </TableHeader>
@@ -52,6 +89,17 @@ const Cart: NextPage<Props> = ({ cartItems }) => {
                   </TableCell>
                   <TableCell>{medicineInShops.medicine.Company.name}</TableCell>
                   <TableCell>{medicineInShops.shop.name}</TableCell>
+                  <TableCell className="text-center">
+                    <Button
+                      variant={"destructive"}
+                      className="h-6 w-5"
+                      onClick={(e) =>
+                        removeItem(e, medicineInShops, cartItem.id)
+                      }
+                    >
+                      X
+                    </Button>
+                  </TableCell>
                   <TableCell className="text-right">
                     {medicineInShops.price}
                   </TableCell>
@@ -79,6 +127,7 @@ export const getServerSideProps = async (ctx: any) => {
   return {
     props: {
       cartItems: res.data.CartItems,
+      cartId: res.data.id,
     },
   };
 };
