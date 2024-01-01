23a8f8300248d27a2f0d2111e5ed0145066a5e1f
Subham Beura <subhambeura20@gmail.com>
2024-01-01 21:09:12 +0530

    add checkout support

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,8 +1,7 @@
-import { NextPage } from "next";
+import { GetServerSideProps } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
 import axios from "axios";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -12,61 +11,34 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useSession } from "next-auth/react";
-import { toast } from "@/components/ui/use-toast";
-import { useState } from "react";
-import { AiFillDelete } from "react-icons/ai";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { AiFillDelete } from "react-icons/ai";
+import { useSession } from "next-auth/react";
 
-interface Props {
-  cartItems: any;
-  cartId: string;
-}
-
-const Cart: NextPage<Props> = ({ cartItems: CI, cartId }) => {
+const checkout = ({ cart }: any) => {
   const { data: session } = useSession();
-  const [cartItems, setCartItems] = useState<any>(CI);
-
+  const { CartItems, id, userId } = cart;
+  console.log(CartItems);
   let totalPrice = 0;
-
-  cartItems.map((cartItem: any, index: number) => {
+  CartItems.map((cartItem: any, index: number) => {
     const medicineInShops: MedicineInShops = cartItem.medicinesInShops;
     totalPrice += medicineInShops.price;
   });
 
-  const removeItem = async (
-    e: any,
-    medicineInShop: MedicineInShops,
-    id: string
-  ) => {
-    console.log(medicineInShop.medicine.name);
-    try {
-      const res = await axios.delete(
-        `${process.env.NEXT_PUBLIC_API_URL}/carts/${cartId}/${id}`,
-        { headers: { Authorization: `Bearer ${session?.user.token}` } }
-      );
-      // filter cartItems such that deleted item is removed
-      setCartItems((prevState: any) => {
-        return prevState.filter((item: any) => item.id !== id);
-      });
-      // window.location.reload();
-      toast({
-        title: "Removed Item From Cart",
-      });
-    } catch (error) {
-      toast({
-        title: "OOPS! Something Went Wrong",
-        description: "Error Removing Item From Cart",
-      });
-    }
+  const order = async () => {
+    const transaction = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/transactions`,
+      { headers: { Authorization: `Bearer ${session?.user.token}` } }
+    );
   };
 
   return (
     <div className="p-10">
       <h1 className="scroll-m-20 text-3xl mb-10 font-extrabold tracking-tight lg:text-3xl">
-        My Cart
+        Checkout
       </h1>
-      {cartItems.length == 0 ? (
+      {CartItems.length == 0 ? (
         "Cart is Empty"
       ) : (
         <Table>
@@ -76,12 +48,11 @@ const Cart: NextPage<Props> = ({ cartItems: CI, cartId }) => {
               <TableHead className="w-[300px]">Medicine</TableHead>
               <TableHead className="w-[300px]">Company</TableHead>
               <TableHead>Shop</TableHead>
-              <TableHead className="text-center">Remove</TableHead>
               <TableHead className="text-right">Amount</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {cartItems.map((cartItem: any, index: number) => {
+            {CartItems.map((cartItem: any, index: number) => {
               const medicineInShops: MedicineInShops =
                 cartItem.medicinesInShops;
               return (
@@ -91,17 +62,7 @@ const Cart: NextPage<Props> = ({ cartItems: CI, cartId }) => {
                   </TableCell>
                   <TableCell>{medicineInShops.medicine.Company.name}</TableCell>
                   <TableCell>{medicineInShops.shop.name}</TableCell>
-                  <TableCell className="text-center">
-                    <Button
-                      variant={"destructive"}
-                      className="p-2 h-8 w-fit"
-                      onClick={(e) =>
-                        removeItem(e, medicineInShops, cartItem.id)
-                      }
-                    >
-                      <AiFillDelete size={20} />
-                    </Button>
-                  </TableCell>
+
                   <TableCell className="text-right">
                     {medicineInShops.price}
                   </TableCell>
@@ -114,15 +75,15 @@ const Cart: NextPage<Props> = ({ cartItems: CI, cartId }) => {
       <div className="w-full flex flex-col justify-end items-end">
         <h3>Total Price: {totalPrice}</h3>
 
-        <Button>
-          <Link href="checkout">Checkout</Link>
+        <Button onClick={order}>
+          <Link href="checkout">Order</Link>
         </Button>
       </div>
     </div>
   );
 };
 
-export const getServerSideProps = async (ctx: any) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerSession(ctx.req, ctx.res, authOptions);
   const res = await axios.get(
     process.env.NEXT_PUBLIC_API_URL + "/carts/" + session?.user.id
@@ -130,9 +91,9 @@ export const getServerSideProps = async (ctx: any) => {
   console.log(res.data);
   return {
     props: {
-      cartItems: res.data.CartItems,
-      cartId: res.data.id,
+      cart: res.data,
     },
   };
 };
-export default Cart;
+
+export default checkout;
